dbe565dd019ad7b12a0d89838b2b564797e8563d
LilMartin <martin_du@qq.com>
2023-09-05 21:23:35 +0800

    feat: 加入使用案例

--- mock-js/src/App.vue
+++ mock-js/src/App.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import axios from 'axios'
+import { ref } from "vue";
+import axios, { AxiosResponse } from "axios";
 
-const data = ref<any>()
-axios.get('/mock/test@method').then((res) => console.log(res))
-axios.get('/mock/test').then((res) => console.log(res))
+const data = ref<any[]>([]);
+axios.get("/mock/test@method").then((res: AxiosResponse) => {
+  data.value = res.data.data;
+});
+axios.get("/mock/test").then((res) => console.log(res));
 </script>
 
 <template>
-  <div>
-    <div>测试 Mock</div>
-    <div>{{ data }}</div>
+  <div class="wrp">
+    <template v-for="item in data" :key="item.id">
+      <div class="module">
+        <template
+          v-for="attr in Object.keys(item).filter((k) => k != 'id')"
+          :key="attr"
+        >
+          <div class="attr">
+            {{ item[attr] }}
+          </div>
+        </template>
+      </div>
+    </template>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrp {
+  display: flex;
+  flex-direction: column;
+}
+.module {
+  display: flex;
+  gap: 20px;
+}
+</style>
